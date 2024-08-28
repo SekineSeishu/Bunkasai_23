1855ab0822f7f8e04b85cbd6d9dafc12be3f6b71
sekineseishu <mika242401@gmail.com>
2024-08-28 15:24:03 +0900

    コインを取った時

---
+++ Assets/seishu/Script/Point.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    private int Addscorepoint = 1;//追加ポイント
+    private AudioSource audio;//SEを流す先
+    [SerializeField] private AudioClip PointSE;//獲得SE
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        //コイン獲得
+        if (collider.gameObject.tag == "Player")
+        {
+            audio.PlayOneShot(PointSE);
+            ScoreManager.Instance.AddScore(Addscorepoint);
+            GetPoint();
+        }
+    }
+
+    //獲得したコインを削除
+    private void GetPoint()
+    { 
+        Destroy(gameObject);
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
