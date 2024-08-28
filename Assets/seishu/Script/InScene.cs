cef6e4897f3f4bd4dd37f3425576adf139883d2a
sekineseishu <mika242401@gmail.com>
2024-08-28 23:10:34 +0900

    シーンの移動

--- Assets/seishu/Script/InScene.cs
+++ Assets/seishu/Script/InScene.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UniRx;
 
 public class InScene : MonoBehaviour
 {
@@ -10,7 +12,9 @@ public class InScene : MonoBehaviour
     public Fade fade;//フェイドインシーン移動
     private AudioSource audio;//SEを流す先
     public AudioClip WaterSE;//シーン移動する際のSE
-    private bool inFade;//フェイドをかけてシーン移動するか判断
+    [SerializeField]private bool inFade;//フェイドをかけてシーン移動するか判断
+
+    private float minInputDuration;//入力を受け付けない
 
     // Start is called before the first frame update
     void Start()
@@ -24,22 +28,25 @@ public class InScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (sceneinput.Player.Scene.triggered)
-        {
-            if (inFade)
+        Observable.EveryAfterUpdate()
+            .Where(_ => sceneinput.Player.Scene.triggered)
+            .ThrottleFirst(System.TimeSpan.FromSeconds(10f))
+            .Subscribe(_ =>
             {
-                audio.PlayOneShot(WaterSE);
-                //トランジションを掛けてシーン遷移する
-                fade.FadeIn(2f, () =>
+                if (inFade)
+                {
+                    audio.PlayOneShot(WaterSE);
+                    //トランジションを掛けてシーン遷移する
+                    fade.FadeIn(2f, () =>
+                    {
+                        SceneManager.LoadScene(LoadScene);
+                        Debug.Log("ボタンが押されました");
+                    });
+                }
+                else
                 {
                     SceneManager.LoadScene(LoadScene);
-                    Debug.Log("ボタンが押されました");
-                });
-            }
-            else
-            {
-                SceneManager.LoadScene(LoadScene);
-            }
-        }
+                }
+            }).AddTo(this);
     }
 }
