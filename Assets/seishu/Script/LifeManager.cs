40e70cc9bfe117e74d02203c881d1c754db3167f
sekineseishu <mika242401@gmail.com>
2024-08-28 15:20:41 +0900

    ライフ、ゲームクリア、ゲームオーバーの管理

--- Assets/seishu/Script/LifeManager.cs
+++ Assets/seishu/Script/LifeManager.cs
@@ -6,26 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class LifeManager : MonoBehaviour
 {
-    private static int Life;
-    private Text lifetext;
-    public GameObject LManager;
-    //リセット用
-    private Vector3 initialPosition;
+    private static int _life;//ライフ
+    private Text lifetext;//ライフ表示
+    public GameObject lifeManager;
+    private Vector3 initialPosition;//リセット位置
     private static LifeManager instance;
     public static LifeManager Instance { get { return instance; } }
 
-    public int life { get { return Life; } }
+    public int life { get { return _life; } }//リザルトシーンでライフを渡す
 
-    public GameObject GameOver;
-    public GameObject VirtulMouse;
-    public GameObject Player;
-    private AudioSource audio;
-    public AudioClip GameOverSE;
-    public GameObject BGM;
-    public GameObject Score;
-    public GameObject ScoreQ;
-    public Text Rtext;
-    public GameObject ScoreInput;
+    [SerializeField] private Text clear;//クリアテキスト
+    [SerializeField] private Text gameOver;//ゲームオーバーオブジェクト
+    public GameObject player;//プレイヤー
+    private AudioSource audio;//SEを流す先
+    public AudioClip gameOverSE;//ゲームオーバーSE
+    public AudioClip clearSE;//ゲームクリアSE
+    public GameObject bgm;//ゲームBGM
+    public GameObject scoreUI;//スコアUI
+    public GameObject closeScoreUI;//リザルト時にスコアを隠す際の隠しUI
+    public Text resultText;
+    public GameObject sceneInput;
     private void Awake()
     {
         initialPosition = transform.position;//保存
@@ -37,56 +37,78 @@ public class LifeManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        DontDestroyOnLoad(LManager);
+        DontDestroyOnLoad(lifeManager);
     }
     // Start is called before the first frame update
     void Start()
     {
-        Life = 5;
-        audio = gameObject.AddComponent<AudioSource>();
-        GameOver.SetActive(false);
-        VirtulMouse.SetActive(false);
-        Player.SetActive(true);
-        BGM.SetActive(true);
-        Score.SetActive(true);
-        ScoreQ.SetActive(false);
-        Rtext.enabled = false;
-        ScoreInput.SetActive(false);
+        _life = 5;//初期ライフ
+
+        //ゲームプレイ状態にする（初期化）
+        audio = gameObject.GetComponent<AudioSource>();
+        clear.enabled = false;
+        gameOver.enabled = false;
+        player.SetActive(true);
+        bgm.SetActive(true);
+        scoreUI.SetActive(true);
+        closeScoreUI.SetActive(false);
+        resultText.enabled = false;
+        sceneInput.SetActive(false);
 
         lifetext = GameObject.Find("Life").GetComponent<Text>();
-        SetLifeText(Life);
+        SetLifeText(_life);
     }
+
+    //ライフを表示
     private void SetLifeText(int life)
     {
         lifetext.text = "×" + life.ToString();
     }
+    //ライフ減少と減少した時にUIを更新
     public void PullLife(int lifePoint)
     {
-        Life -= lifePoint;
-        SetLifeText(Life);
+        _life -= lifePoint;
+        SetLifeText(_life);
     }
-    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "goal")
+        {
+            //クリアテキストとリザルト画面を表示
+            clear.enabled = true;
+            audio.PlayOneShot(clearSE);
+            Result();
+        }
+    }
+
+    //リザルトUI
+    public void Result()
+    {
+        player.SetActive(false);//プレイヤーを画面から消す
+        bgm.SetActive(false);//ゲームBGMを止める
+        scoreUI.SetActive(false);//スコア表示を消す
+        closeScoreUI.SetActive(true);//スコアの表示が？のUIを表示
+        resultText.enabled = true;
+        sceneInput.SetActive(true);
+    }
+
     void Update()
     {
-        if (Life == 0)
+        if (_life == 0)
         {
-            //ゲームオーバーSE
-            audio.PlayOneShot(GameOverSE);
-            GameOver.SetActive(true);
-            VirtulMouse.SetActive(true);
-            Player.SetActive(false);
-            BGM.SetActive(false);
-            Score.SetActive(false);
-            ScoreQ.SetActive(true);
-            Rtext.enabled = true;
-            ScoreInput.SetActive(true);
-            Life = -1;
-
+            audio.PlayOneShot(gameOverSE);//ゲームオーバーSE
+            gameOver.enabled = true;//ゲームオーバーテキストを出す
+            player.SetActive(false);//プレイヤーを画面から消す
+            bgm.SetActive(false);//ゲームBGMを止める
+            scoreUI.SetActive(false);//スコア表示を消す
+            closeScoreUI.SetActive(true);//スコアの表示が？のUIを表示
+            resultText.enabled = true;
+            sceneInput.SetActive(true);
         }
         //SceneManagerの名前がタイトルの場合にScoreManagerを破棄
         if (SceneManager.GetActiveScene().name == "titol")
         {
-            Destroy(LManager);
+            Destroy(lifeManager);
         }
     }
 
