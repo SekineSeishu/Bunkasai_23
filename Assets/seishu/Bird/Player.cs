b73d17886ba5d215a57bf6b9f304fbd5ee572044
SekineSeishu <mika242401@gmail.com>
2024-08-28 12:20:49 +0900

    プレイヤーの不要部分の削除

--- Assets/seishu/Bird/Player.cs
+++ Assets/seishu/Bird/Player.cs
@@ -81,32 +81,5 @@ public class Player : MonoBehaviour
                 particles.Stop();// キャラクターが動いている場合はParticleを停止する
             }
         }
-
     }
-
-    /* 敵に当たった時に時間を遅くする処理を追加
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            // 時間を遅くする効果のスクリプトを持つオブジェクトを取得し、そのスクリプトを実行する
-            Damege timeSlowEffect = FindObjectOfType<Damege>();
-            if (timeSlowEffect != null)
-            {
-                timeSlowEffect.TriggerTimeSlow();
-
-                // プレイヤーの速度を遅くする
-                Speed = playerSlowSpeed;
-                ResetSpeed();
-                Debug.Log("う、動けん！");
-            }
-        }
-    }
-     //時間を元に戻す際に速度を元に戻す
-    public void ResetSpeed()
-    {
-        Speed = setSpeed;
-        Debug.Log("う、動けん！");
-    }*/
-
 }
