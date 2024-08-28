d8176356afae35e5714cf9999c435811684088db
sekineseishu <mika242401@gmail.com>
2024-08-28 15:25:24 +0900

    プレイヤーを追尾するカメラ

--- Assets/seishu/Script/CameraManager.cs
+++ Assets/seishu/Script/CameraManager.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    GameObject player;
+    [SerializeField] private GameObject player;
     public float cameraZ = -17.3f;
     // Start is called before the first frame update
     void Start()
     {
-        this.player = GameObject.Find("Player");
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //プレイヤーを常に中心にする
         Vector3 playerPos = this.player.transform.position;
         transform.position = new Vector3(playerPos.x, playerPos.y, cameraZ);
     }
